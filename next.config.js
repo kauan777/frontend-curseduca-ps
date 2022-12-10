911425adde672a18c4e0a0032dd868ccf11c0450
kauan777 <kauan7622@gmail.com>
2022-12-10 08:57:40 -0300

    add images domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["localhost", "images.cws.digital"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
